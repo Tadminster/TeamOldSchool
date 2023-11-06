a6ba06e9b989b853c175b612bb2a5dac435305aa
MinGyeongHo <rudghgmrdls1@gmail.com>
2023-11-07 00:58:27 +0900

    메인슬롯상시노출 인벤토리 켜기끄기

--- Valheim/InGame.cpp
+++ Valheim/InGame.cpp
@@ -11,33 +11,37 @@
 
 InGame::InGame()
 {
-    grid = Grid::Create("Grid");
+	grid = Grid::Create("Grid");
 
-    tempCamera = Camera::Create("tempCamera");
-    tempCamera->LoadFile("Cam.xml");
-    Camera::main = tempCamera;
+	tempCamera = Camera::Create("tempCamera");
+	tempCamera->LoadFile("Cam.xml");
+	Camera::main = tempCamera;
 
-    map = Terrain::Create("Terrain");
-    map->LoadFile("Terrain.xml");
-    map->CreateStructuredBuffer();
+	map = Terrain::Create("Terrain");
+	map->LoadFile("Terrain.xml");
+	map->CreateStructuredBuffer();
 
-    skyBox = Sky::Create();
-    skyBox->LoadFile("Sky1.xml");
+	skyBox = Sky::Create();
+	skyBox->LoadFile("Sky1.xml");
 
-    //RESOURCE->shaders.Load("0.Sky_CR.hlsl")->LoadGeometry();
-    //RESOURCE->shaders.Load("0.SkySphere_CR.hlsl")->LoadGeometry();
-    //RESOURCE->shaders.Load("5.Cube_CR.hlsl")->LoadGeometry();
-    //RESOURCE->shaders.Load("5.Cube_Shadow.hlsl")->LoadGeometry();
-    //RESOURCE->shaders.Load("4.Instance_CR.hlsl")->LoadGeometry();
-    //RESOURCE->shaders.Load("4.Instance_Shadow.hlsl")->LoadGeometry();
+	//RESOURCE->shaders.Load("0.Sky_CR.hlsl")->LoadGeometry();
+	//RESOURCE->shaders.Load("0.SkySphere_CR.hlsl")->LoadGeometry();
+	//RESOURCE->shaders.Load("5.Cube_CR.hlsl")->LoadGeometry();
+	//RESOURCE->shaders.Load("5.Cube_Shadow.hlsl")->LoadGeometry();
+	//RESOURCE->shaders.Load("4.Instance_CR.hlsl")->LoadGeometry();
+	//RESOURCE->shaders.Load("4.Instance_Shadow.hlsl")->LoadGeometry();
 	playerInventory = UI::Create("playerInventory");
 	playerInventory->LoadFile("InvenUI.xml");
-	playerInventory->visible = false;
-    //첫번째 메인 사용아이템 ui는 인벤토리를 꺼도 보입니다!
-    for (int i = 0; i < 8; i++)
-    {
-        playerInventory->Find("mainSlot" + to_string(i))->visible = true;
-    }
+	//인벤토리 처음에 감추기 메인슬롯은 보여줍니다
+	playerInventory->Find("PLAYER_PANNEL_INVEN")->visible = false;
+	for (int i = 0; i < 8; i++)
+	{
+		playerInventory->Find("mainSlot" + to_string(i))->visible = true;
+		playerInventory->Find("subSlot" + to_string(i))->visible = false;
+		playerInventory->Find("SecondsubSlot" + to_string(i))->visible = false;
+		playerInventory->Find("ThirdsubSlot" + to_string(i))->visible = false;
+	}
+
 
 	RESOURCE->shaders.Load("0.Sky_CR.hlsl")->LoadGeometry();
 }
@@ -57,24 +61,24 @@ void InGame::Release()
 
 void InGame::Update()
 {
-    LIGHT->RenderDetail();
+	LIGHT->RenderDetail();
 
-    Camera::main->ControlMainCam();
-    Camera::main->Update();
+	Camera::main->ControlMainCam();
+	Camera::main->Update();
 
-    ImGui::Text("FPS: %d", TIMER->GetFramePerSecond());
-    ImGui::Begin("Hierarchy");
-    {
-        grid->RenderHierarchy();
-        tempCamera->RenderHierarchy();
-        map->RenderHierarchy();
-        skyBox->RenderHierarchy();
-        playerInventory->RenderHierarchy();
-    }
-    ImGui::End();
-	
+	ImGui::Text("FPS: %d", TIMER->GetFramePerSecond());
+	ImGui::Begin("Hierarchy");
+	{
+		grid->RenderHierarchy();
+		tempCamera->RenderHierarchy();
+		map->RenderHierarchy();
+		skyBox->RenderHierarchy();
+		playerInventory->RenderHierarchy();
+	}
+	ImGui::End();
 
-	
+
+
 	grid->Update();
 	skyBox->Update();
 	playerInventory->Update();
@@ -82,7 +86,7 @@ void InGame::Update()
 	//ui 위치코드로 배정해주기
 	for (int i = 0; i < 8; i++)
 	{
-		playerInventory->Find("subSlot" + to_string(i))->SetLocalPos(Vector3(-0.320 + i * 0.090,  0.110, 0));
+		playerInventory->Find("subSlot" + to_string(i))->SetLocalPos(Vector3(-0.320 + i * 0.090, 0.110, 0));
 		playerInventory->Find("subSlot" + to_string(i))->scale.x = 0.110f;
 		playerInventory->Find("subSlot" + to_string(i))->scale.y = 0.170f;
 
@@ -103,28 +107,34 @@ void InGame::LateUpdate()
 
 	if (INPUT->KeyDown(VK_TAB))
 	{
-		playerInventory->visible = !playerInventory->visible;
+		playerInventory->Find("PLAYER_PANNEL_INVEN")->visible = !playerInventory->Find("PLAYER_PANNEL_INVEN")->visible;
+		for (int i = 0; i < 8; i++)
+		{
+			playerInventory->Find("subSlot" + to_string(i))->visible = !playerInventory->Find("subSlot" + to_string(i))->visible;
+			playerInventory->Find("SecondsubSlot" + to_string(i))->visible = !playerInventory->Find("SecondsubSlot" + to_string(i))->visible;
+			playerInventory->Find("ThirdsubSlot" + to_string(i))->visible = !playerInventory->Find("ThirdsubSlot" + to_string(i))->visible;
+		}
 	}
-	
+
 
 }
 
 void InGame::PreRender()
 {
-    LIGHT->Set();
+	LIGHT->Set();
 
-    skyBox->Render(RESOURCE->shaders.Load("0.Sky_CR.hlsl"));
-    map->Render(RESOURCE->shaders.Load("5.Cube_CR.hlsl"));
+	skyBox->Render(RESOURCE->shaders.Load("0.Sky_CR.hlsl"));
+	map->Render(RESOURCE->shaders.Load("5.Cube_CR.hlsl"));
 }
 
 void InGame::Render()
 {
-    Camera::main->Set();
-    LIGHT->Set();
+	Camera::main->Set();
+	LIGHT->Set();
 
-    skyBox->Render();
-    grid->Render();
-    map->Render();
+	skyBox->Render();
+	grid->Render();
+	map->Render();
 
 	playerInventory->Render();
 
@@ -132,11 +142,11 @@ void InGame::Render()
 
 void InGame::ResizeScreen()
 {
-    Camera::main->viewport.x = 0.0f;
-    Camera::main->viewport.y = 0.0f;
-    Camera::main->viewport.width = App.GetWidth();
-    Camera::main->viewport.height = App.GetHeight();
+	Camera::main->viewport.x = 0.0f;
+	Camera::main->viewport.y = 0.0f;
+	Camera::main->viewport.width = App.GetWidth();
+	Camera::main->viewport.height = App.GetHeight();
 
-    Camera::main->width = App.GetWidth();
-    Camera::main->height = App.GetHeight();
+	Camera::main->width = App.GetWidth();
+	Camera::main->height = App.GetHeight();
 }
