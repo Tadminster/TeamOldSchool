c627f162cadcccc856490289d459f528ce8dc09e
biBeam <lovewonvv@gamil.com>
2023-11-08 17:17:45 +0900

    플레이어 z축 수정

--- Valheim/PlayerState.cpp
+++ Valheim/PlayerState.cpp
@@ -12,9 +12,9 @@ void IdleState::Idle()
 
 void IdleState::Move()
 {
-	player->GetPlayer()->MoveWorldPos(player->GetPlayer()->GetForward() * 10.0f * DELTA);
-	player->GetPlayer()->
-		anim->ChangeAnimation(AnimationState::LOOP, 1);
+	PLAYER->GetPlayer()->MoveWorldPos(PLAYER->GetPlayer()->GetForward() * 10.0f * DELTA);
+	/*PLAYER->GetPlayer()->
+		anim->ChangeAnimation(AnimationState::LOOP, 1);*/
 	SetPlayerState(MoveState::GetInstance());
 }
 
@@ -27,7 +27,7 @@ void MoveState::Idle()
 
 void MoveState::Move()
 {
-	player->GetPlayer()->MoveWorldPos(player->GetPlayer()->GetForward() * 10.0f * DELTA);
-	player->GetPlayer()->
-		anim->ChangeAnimation(AnimationState::LOOP, 1);
+	PLAYER->GetPlayer()->MoveWorldPos(PLAYER->GetPlayer()->GetForward() * 10.0f * DELTA);
+	/*PLAYER->GetPlayer()->
+		anim->ChangeAnimation(AnimationState::LOOP, 1);*/
 }
