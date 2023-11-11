25d85b99750382d33b1fc9d5674ea250e340be0d
Tadminster <91bottlenine@naver.com>
2023-11-11 18:41:05 +0900

    지형이 생성되는 방식이 변경됨에 따라 나무 생성하는 logic 변경(높이값 체크와 군락을 이루도록)

--- Valheim/ObjectManager.cpp
+++ Valheim/ObjectManager.cpp
@@ -108,55 +108,56 @@ void ObjectManager::RenderHierarchy()
 void ObjectManager::GenerateFeatures()
 {
 	int rowSize = MAP->rowSize;
-	double frequencyScale = 1.0 / rowSize * 2;		// 맵 크기에 따른 주파수 스케일 조정
-	double centerPeak = 2.0;						// 중앙 높이 증가
-	double edgeSteepness = 10.0;					// 가장자리 경사의 가파름 조절
-
-	int randomSeed = RANDOM->Int(0, 10000);			// 난수 시드
-	siv::PerlinNoise perlin(randomSeed);			// 퍼린 노이즈 생성
+	double frequencyScale = 1.0 / rowSize * 2;			// 맵 크기에 따른 주파수 스케일 조정
+	siv::PerlinNoise perlin(RANDOM->Int(0, 10000));		// 난수 시드로 펄린노이즈 생성
 
 	VertexTerrain* vertices = (VertexTerrain*)MAP->mesh->vertices;
-	static int count = 0;
+	Ray ray; Vector3 Hit;
+
 	for (int i = 0; i < rowSize; i++)
 	{
 		for (int j = 0; j < rowSize; j++)
 		{
+			float positionY = vertices[i * rowSize + j].position.y;
+
+			// 높이체크
+			// 높이가 -1 이하 인곳에서는 생성하지 않음
+			if (positionY < -1.0f) continue;
+			// 높이가 -1 ~ 1 이면, 50분의 1 확률로 생성
+			else if (positionY < 1.0f)
+			{
+				if (RANDOM->Int(1, 50) != 1) continue;
+			}
+			// 높이가 1 ~ 10 이면, 30분의 1 확률로 생성
+			else if(positionY < 10.0f)
+			{
+				if (RANDOM->Int(1, 30) != 1) continue;
+			}
+			else // 높이가 10 이면, 20분의 1 확률로 생성
+			{
+				if (RANDOM->Int(1, 20) != 1) continue;
+			}
+
 			double x = (double)i * frequencyScale;
 			double y = (double)j * frequencyScale;
 			double z = 0.5;
 			double noiseValue = perlin.noise3D(x, y, z);
 
-			// 중앙에서의 거리에 따라 가중치 적용
-			double maxDistance = sqrt(2) * (rowSize / 2.0);
-
-			// 각 꼭짓점에서 가장 가까운 가장자리까지의 최소 거리 계산
-			double minEdgeDistance = min(min(i, rowSize - 1 - i), min(j, rowSize - 1 - j));
-			double edgeFactor = pow((maxDistance - minEdgeDistance) / maxDistance, edgeSteepness) - 1;
-
-			// 최종 높이 계산
-			float result = noiseValue - edgeFactor;
-
-			if (result > 1.0f || result > RANDOM->Float(0, 1))
+			// 펄린노이즈 값 체크
+			if (noiseValue > -0.2f)
 			{
-				if (RANDOM->Int(1, 50) == 1)
+				ray.position = vertices[i * rowSize + j].position; +Vector3(0, 1000, 0);
+				ray.direction = Vector3(0, -1, 0);
+				if (MAP->ComPutePicking(ray, Hit))
 				{
-					Ray ray;
-					ray.position = vertices[i * rowSize + j].position; +Vector3(0, 1000, 0);
-					ray.direction = Vector3(0, -1, 0);
-					Vector3 Hit;
-					if (MAP->ComPutePicking(ray, Hit))
-					{
-						TreeBeech* treeBeech = new TreeBeech(Hit, RenderType::SINGLE);
-						treeBeech->GetActor()->rotation.y = RANDOM->Float(0, 360) * ToRadian;
-						treeBeech->GetActor()->scale = Vector3(RANDOM->Float(0.002f, 0.003f), RANDOM->Float(0.002f, 0.003f), RANDOM->Float(0.002f, 0.003f));
-						objects.emplace_back(treeBeech);
-						count++;
-					}
+					TreeBeech* treeBeech = new TreeBeech(Hit, RenderType::SINGLE);
+					treeBeech->GetActor()->rotation.y = RANDOM->Float(0, 360) * ToRadian;
+					treeBeech->GetActor()->scale = Vector3(RANDOM->Float(0.002f, 0.003f), RANDOM->Float(0.002f, 0.003f), RANDOM->Float(0.002f, 0.003f));
+					objects.emplace_back(treeBeech);
 				}
 			}
 		}
 	}
-	cout << "생성된 나무 수:" << count;
 }
 
 void ObjectManager::GenerateInstanceFeature()
