716e11e3317b016f5d0b2741c93e919ff45a674b
Tadminster <91bottlenine@naver.com>
2023-11-13 12:56:21 +0900

    Prototype에 오브젝트가 파괴되는 조건을 검사하는 함수 DestructionEvent 추가

--- Valheim/Prototype.h
+++ Valheim/Prototype.h
@@ -17,8 +17,13 @@ public:
 	virtual void LateUpdate() = 0;
 	virtual void Render() = 0;
 	virtual void Release() = 0;
+	virtual void RenderHierarchy() = 0;
 
 	virtual Actor* GetActor() { return actor; }
-	virtual void RenderHierarchy() = 0;
+
+	// @brief 오브젝트가 파괴되는 조건을 검사하는 함수
+	// @brief 예를 들어, 체력이 0 이하이거나 아이템이 땅에 드랍되고 일정시간이 지났을 때
+	// @return 파괴되어야 하는 경우 true, 아닌 경우 false
+	virtual bool DestructionEvent() = 0;
 };
 
