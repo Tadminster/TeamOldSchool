198aa3c828f8bda7724f721348b1bf181aa90af3
Tadminster <91bottlenine@naver.com>
2023-11-15 00:08:54 +0900

    아이템의 중간 부모클래스 추가

--- Valheim/ItemProto.h
+++ Valheim/ItemProto.h
@@ -1,12 +1,13 @@
 #pragma once
 
+//====================================================================================================
 enum class ItemState { OnGround, OnInventory };
 enum class ItemType  { Weapon, Tool, Armor, Food, Material };
 enum class ItemName 
 { 
 	StoneAxe 
 };
-
+//====================================================================================================
 class ItemProto : public Prototype
 {
 protected:
@@ -20,12 +21,12 @@ public:
 	virtual ~ItemProto() {};
 	virtual void Use() = 0;
 };
-
+//====================================================================================================
 class ToolProto : public ItemProto
 {
 
 };
-
+//====================================================================================================
 class WeaponProto : public ItemProto
 {
 protected:
@@ -35,11 +36,26 @@ protected:
 public:
 	virtual void Fix() = 0;
 };
+//====================================================================================================
+class ArmorProto : public ItemProto
+{
+protected:
+	int		defense;		// 방어력
+	int		durability;		// 내구도
 
+public:
+	virtual void Fix() = 0;
+};
+//====================================================================================================
 class FoodProto : public ItemProto
 {
 protected:
 	int duration;					// 지속시간	
 	int hitPointRecoveryAmount;		// 체력 회복량
 	int staminaPointRecoveryAmount;	// 스태미너 회복량
+};
+//====================================================================================================
+class MaterialProto : public ItemProto
+{
+
 };
