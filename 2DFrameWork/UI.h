2884cf4026e400d5e92f4d9b3bc291d3a4474ba8
Tadminster <91bottlenine@naver.com>
2023-11-23 00:23:17 +0900

    컨텐츠 추가(돌, 빈이미지) 및 수정(크래프팅창)

--- 2DFrameWork/UI.h
+++ 2DFrameWork/UI.h
@@ -13,10 +13,10 @@ public:
 	bool			MouseDown(class Camera* cam = Camera::main);
 	void			RenderDetail();
 
-	function<void()> mouseOver = nullptr; //마우스가 위에존재할떄
-	function<void()> mouseDown = nullptr; // 위에서 눌렀을때 한번
-	function<void()> mousePress = nullptr; // 누르고 있을때
-	function<void()> mouseUp = nullptr; //누르고 떼었을때
+	function<void()> mouseOver = nullptr;	// 마우스가 위에 존재할 때
+	function<void()> mouseDown = nullptr;	// 마우스가 위에 존재하고, 클릭했을 때
+	function<void()> mousePress = nullptr;	// 누르고 있을 때
+	function<void()> mouseUp = nullptr;		// 누르고 떼었을 때
 
 	virtual void	Update() override;
 	void			Render(shared_ptr<Shader> pShader = nullptr);
