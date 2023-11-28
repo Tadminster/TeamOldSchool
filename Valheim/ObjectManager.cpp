b9628137c2084b1f6a7ba0e6b5f9da001c402b21
biBeam <lovewonvv@gamil.com>
2023-11-28 21:55:14 +0900

    오브젝트 - 플레이어 거리 5이하일때만 충돌검사 시행 (프레임40~50 이득)

--- Valheim/ObjectManager.cpp
+++ Valheim/ObjectManager.cpp
@@ -126,10 +126,24 @@ void ObjectManager::LateUpdate()
 	// 오브젝트에 데미지 입히기
 	for (auto& obj : objects)
 	{
-		if (PLAYER->CleanHit(obj->GetActor()->collider) && PLAYER->CleanFrame())
+		//오브젝트 - 플레이어 거리 5이하일때만 충돌검사 시행
+		if ((PLAYER->GetPlayer()->GetWorldPos() - obj->GetActor()->GetWorldPos()).Length() <= 5.0f)
 		{
-			obj->ReceivedDamageEvent(PLAYER->GetWeaponDMG(), PLAYER->GetWeaponType());
-			break;
+			if (PLAYER->CleanHit(obj->GetActor()->collider) && PLAYER->CleanFrame())
+			{
+				obj->ReceivedDamageEvent(PLAYER->GetWeaponDMG(), PLAYER->GetWeaponType());
+				break;
+			}
+			//오브젝트 - 플레이어 슬라이딩 벡터 조건
+			if (PLAYER->GetCollider()->Intersect(obj->GetActor()->collider))
+			{
+				PLAYER->istouch = true;
+				PLAYER->MoveBack(obj->GetActor());
+			}
+			else
+			{
+				PLAYER->istouch = false;
+			}
 		}
 	}
 }
