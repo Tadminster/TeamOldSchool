f4ba436abc99eaba7fa375edd562eca01061ccba
Tadminster <91bottlenine@naver.com>
2023-11-30 23:24:21 +0900

    나무, 돌 y scale 조절, 난파선 1개 OBJ->Init()에서 추가

--- Valheim/Beech.cpp
+++ Valheim/Beech.cpp
@@ -11,9 +11,9 @@ Beech::Beech()
 	actor->LoadFile("Feature_Beech.xml");
 	actor->name = "Beech" + to_string(index++);
 
-	float x = RANDOM->Float(0.8f, 1.2f);
-	float y = RANDOM->Float(0.6f, 0.8f);
-	float z = RANDOM->Float(0.8f, 1.2f);
+	float x = RANDOM->Float(1.0f, 1.2f);
+	float y = RANDOM->Float(0.8f, 1.0f);
+	float z = RANDOM->Float(1.0f, 1.2f);
 	actor->rotation.y = RANDOM->Float(0.0f, 360.0f) * ToRadian;
 	actor->scale = Vector3(x, y, z);
 
@@ -37,7 +37,7 @@ void Beech::Update()
 	//오브젝트와 카메라의 거리 계산
 	playerDistance = Vector3::DistanceSquared(PLAYER->GetActor()->GetWorldPos(), actor->GetWorldPos());
 	
-	// 거리가 3000.0f 이상이면 리턴(업데이트 하지 않음)
+	// 거리가 2000.0f 이상이면 리턴(업데이트 하지 않음)
 	if (playerDistance > MAXMUM_UPDATE_DISTANCE) return;
 
 	LodUpdate(playerDistance);
@@ -72,9 +72,9 @@ void Beech::LodUpdate(float distance)
 	actor->Find("Lod1")->visible = false;
 	actor->Find("Lod3")->visible = false;
 
-	if (distance < 1000) actor->Find("Lod0")->visible = true;			// 거리 1000 이하는 LOD0
-	else if (distance < 2000) actor->Find("Lod1")->visible = true;		// 거리 2000 이하는 LOD1
-	else if (distance < 3000) actor->Find("Lod3")->visible = true;		// 거리 5000 이하는 LOD3
+	if (distance < 500) actor->Find("Lod0")->visible = true;			// 거리 1000 이하는 LOD0
+	else if (distance < 1000) actor->Find("Lod1")->visible = true;		// 거리 2000 이하는 LOD1
+	else if (distance < 2000) actor->Find("Lod3")->visible = true;		// 거리 5000 이하는 LOD3
 	else return;
 }
 
