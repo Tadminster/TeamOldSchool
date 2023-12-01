e511f201c6fa0b87bdb7b7d5b93c132708e56625
MinGyeongHo <rudghgmrdls1@gmail.com>
2023-12-02 03:05:13 +0900

    오션 추가

--- Shaders/5.Ocean.hlsl
+++ Shaders/5.Ocean.hlsl
@@ -3,84 +3,80 @@
 struct VertexInput
 {
     float4 Position : POSITION0;
-	float2 Uv : UV0;
-	float3 Normal : NORMAL0;
-	float4 Weights : WEIGHTS0;
-    float4 Color : COLOR0;
+    float2 Uv : UV0;
+    float3 Normal : NORMAL0;
+    float4 Weights : WEIGHTS0;
 };
 struct PixelInput
 {
     float4 Position : SV_POSITION;
-	float2 Uv : UV0;
-	float4 wPosition : POSITION0;
-	float3 Normal : NORMAL0;
-	float4 Weights : WEIGHTS0;
-	float4 Color : COLOR0;
+    float2 Uv : UV0;
+    float4 wPosition : POSITION0;
+    float3 Normal : NORMAL0;
+    float4 Weights : WEIGHTS0;
 };
 
 PixelInput VS(VertexInput input)
 {
    
     PixelInput output;
-	output.Uv = input.Uv;
+    output.Uv = input.Uv;
     //  o           =  i X W
     output.Position = mul(input.Position, World);
-	output.wPosition = output.Position;
-	output.Position = mul(output.Position, View);
-	output.Position = mul(output.Position, Proj);
-	output.Normal = mul(input.Normal, (float3x3) World);
-	output.Weights = input.Weights;
-    output.Color = input.Color;
+    output.wPosition = output.Position;
+    output.Position = mul(output.Position, View);
+    output.Position = mul(output.Position, Proj);
+    output.Normal = mul(input.Normal, (float3x3) World);
+    output.Weights = input.Weights;
     return output;
 }
 
 
 
-float4 PS(PixelInput input):SV_TARGET
+float4 PS(PixelInput input) : SV_TARGET
 {
-    float4 BaseColor = input.Color;
-	//float4 BaseColor = 0;
-	BaseColor.a = 1.0f;
+    float4 BaseColor = 0;
+    BaseColor.a = 1.0f;
 	  [flatten]
-	if (Ka.a)
-		BaseColor.rgb += TextureN.Sample(SamplerN, input.Uv).rgb * input.Weights.x;
+    if (Ka.a)
+        BaseColor.rgb += TextureN.Sample(SamplerN, input.Uv).rgb * input.Weights.x;
 	  [flatten]
-	if (Kd.a)
-		BaseColor.rgb += TextureD.Sample(SamplerD, input.Uv).rgb * input.Weights.y;
+    if (Kd.a)
+        BaseColor.rgb += TextureD.Sample(SamplerD, input.Uv).rgb * input.Weights.y;
       [flatten]
-	if (Ks.a)
-		BaseColor.rgb += TextureS.Sample(SamplerS, input.Uv).rgb * input.Weights.z;
+    if (Ks.a)
+        BaseColor.rgb += TextureS.Sample(SamplerS, input.Uv).rgb * input.Weights.z;
       [flatten]
-	if (Ke.a)
-		BaseColor.rgb += TextureE.Sample(SamplerE, input.Uv).rgb * input.Weights.w;
+    if (Ke.a)
+        BaseColor.rgb += TextureE.Sample(SamplerE, input.Uv).rgb * input.Weights.w;
 	
 	//BaseColor = float4(input.Weights.w, input.Weights.w, input.Weights.w, 1);
 	//
 	
-	float3 SpecularMap = float3(1, 1, 1);
+    float3 SpecularMap = float3(1, 1, 1);
 	
-	BaseColor = float4(DirLighting(BaseColor.rgb, SpecularMap,
+    BaseColor = float4(DirLighting(BaseColor.rgb, SpecularMap,
     normalize(input.Normal), input.wPosition.xyz),
     BaseColor.a);
 	
-	for (int i = 0; i < lights[0].Size; i++)
-	{
+    for (int i = 0; i < lights[0].Size; i++)
+    {
         [flatten]
-		if (!lights[i].isActive)
-			continue;
+        if (!lights[i].isActive)
+            continue;
         
         [flatten]
-		if (lights[i].Type == 0)
-		{
-			BaseColor.rgb += PointLighting(BaseColor.rgb, SpecularMap,
+        if (lights[i].Type == 0)
+        {
+            BaseColor.rgb += PointLighting(BaseColor.rgb, SpecularMap,
 			input.Normal, input.wPosition.xyz, i);
-		}
-		else if (lights[i].Type == 1)
-		{
-			BaseColor.rgb += SpotLighting(BaseColor.rgb, SpecularMap,
+        }
+        else if (lights[i].Type == 1)
+        {
+            BaseColor.rgb += SpotLighting(BaseColor.rgb, SpecularMap,
             input.Normal, input.wPosition.xyz, i);
-		}
-	}
+        }
+    }
 	
 	
     BaseColor.rgb += Ka.rgb * BaseColor.rgb;
